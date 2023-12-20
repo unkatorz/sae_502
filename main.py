272154a4a8ce834737d0a438e2de34469992f991
Matthieu Honoré <matthieuhonore@dn040213.etu.univ-rennes1.fr>
2023-12-20 11:57:12 +0100

    Changement du fichier audit.html et execution de la commande lynis audit system

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
-
+from flask import Flask, request, render_template
+import subprocess
+import re
 app = Flask(__name__)
+
+def remove_ansi_color_codes(text):
+    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+    return ansi_escape.sub('', text)
 
 @app.route('/')
 def home():
@@ -17,6 +22,21 @@ def audit():
 @app.route('/ssl')
 def ssl():
     return render_template('ssl.html')
+
+@app.route('/execute_command', methods=['POST'])
+def execute_command():
+    if request.method == 'POST':
+        command = request.form['command']
+        if command.strip() == 'lynis audit system':
+            try:
+                result = subprocess.check_output(command, shell=True, text=True)
+                resultat = remove_ansi_color_codes(result)
+                # Utiliser Markup pour conserver la mise en forme HTML
+                return "<pre>{}</pre>".format(resultat)
+            except Exception as e:
+                return str(e)
+        else:
+            return "Commande non autorisée."
 
 if __name__=='__main__':
     app.run()
